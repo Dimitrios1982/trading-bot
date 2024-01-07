b4ae8f7f7a2f532fcfbf56a83bfef68fdd568bc7
Dimitrios1982 <145916245+Dimitrios1982@users.noreply.github.com>
2024-01-07 01:24:55 +0100

    m

--- iexcloud_api_handler.py
+++ iexcloud_api_handler.py
@@ -13,8 +13,10 @@ def get_data(workspace, dataset_id,symbol, params, sec_filing_type= None):
     params = _encode_params(params)
     if sec_filing_type == None:
         base_url = f"https://api.iex.cloud/v1/data/{workspace}/{dataset_id}/{symbol}?{params}"
+        file_path = os.path.join('Data', f'{symbol}_{workspace}_{dataset_id}.csv')
     elif sec_filing_type != None:
         base_url = f"https://api.iex.cloud/v1/data/{workspace}/{dataset_id}/{symbol}/{sec_filing_type}?{params}"
+        file_path = os.path.join('Data', f'{symbol}_{workspace}_{dataset_id}_{sec_filing_type}.csv')
     print(base_url)
     response = requests.get(base_url)
     
--- iexcloud_preprocessing.py
+++ iexcloud_preprocessing.py
@@ -1,29 +1,26 @@
 import pandas as pd
 import json
 
-prices_df = pd.read_csv('Data/TSLA_core_historical_prices.csv')
+file_path = 'Data/'
+file_name_prices = 'TSLA_core_historical_prices.csv'
+prices_df = pd.read_csv(f'{file_path}{file_name_prices}')
+
+file_name_sec = 'TSLA_core_reported_financials.csv'
+sec_filings_df = pd.read_csv(f'{file_path}{file_name_sec}')
+
 print(prices_df.head(5))
+print(sec_filings_df)
 
 sec_filings_columns = [
-    "Revenue",
-    "NetIncomeLoss",
-    "EarningsPerShareBasic",
-    "EarningsPerShareDiluted",
-    "OperatingIncomeLoss",
-    "CostOfRevenue",
-    "GrossProfit",
-    "CashAndCashEquivalentsAtCarryingValue",
-    "LongTermDebt",
-    "DebtCurrent",
-    "Assets",
-    "LiabilitiesCurrent",
-    "LiabilitiesNoncurrent",
-    "FreeCashFlow",
-    "ROE",
-    "Dividends",
-    "InventoryNet",
-    "ResearchAndDevelopmentExpense",
-    "MarketCapitalization",
-    "StockRepurchasedAndRetiredDuringPeriodValue",
-    "ShareBasedCompensation"
+    "Revenues", "NetIncomeLoss", "EarningsPerShareBasic", "EarningsPerShareDiluted", 
+    "OperatingIncomeLoss", "CostOfRevenue", "GrossProfit", 
+    "CashAndCashEquivalentsAtCarryingValue", "LongTermDebt", "DebtCurrent", 
+    "Assets", "LiabilitiesCurrent", "InventoryNet", "ResearchAndDevelopmentExpense"
 ]
+
+sec_filings_df = sec_filings_df[sec_filings_columns]
+sec_filings_df.to_csv(f'{file_path}{file_name_sec}_processed.csv', index=False)
+print(sec_filings_df)
+
+
+
