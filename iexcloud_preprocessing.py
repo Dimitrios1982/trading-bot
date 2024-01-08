e64d5d59980550980da25d5a8f718b9de6444eba
Dimitrios1982 <145916245+Dimitrios1982@users.noreply.github.com>
2024-01-08 22:41:45 +0100

    rnn model finished

--- iexcloud_preprocessing.py
+++ iexcloud_preprocessing.py
@@ -1,17 +1,22 @@
 import pandas as pd
-import json
+import numpy as np
 
 file_path = 'Data/'
+
+# Preprocess stock prices
 file_name_prices = 'TSLA_core_historical_prices.csv'
 prices_df = pd.read_csv(f'{file_path}{file_name_prices}')
 
-prices_columns = ["date"] #continue
+prices_columns = ["priceDate", "open", "low", "high", "close", "volume"]
 
+prices_df = prices_df[prices_columns]
+date_column = prices_df["priceDate"] 
+prices_df["priceDate"] = pd.to_datetime(date_column).astype(np.int64)    # Convert the date column to a numeric representation (e.g., timestamp)
+prices_df.to_csv(f'{file_path}{file_name_prices}_preprocessed.csv', index=False)
+
+# Preprocess SEC filings
 file_name_sec = 'TSLA_core_reported_financials.csv'
 sec_filings_df = pd.read_csv(f'{file_path}{file_name_sec}')
-
-print(prices_df.head(5))
-print(sec_filings_df)
 
 sec_filings_columns = [
     "Revenues", "NetIncomeLoss", "EarningsPerShareBasic", "EarningsPerShareDiluted", 
@@ -30,5 +35,5 @@ sec_filings_repeated = pd.concat([sec_filings_df] * len(prices_df), ignore_index
 # Merge the two DataFrames
 merged_df = pd.concat([prices_df, sec_filings_repeated], axis=1)
 merged_df.to_csv(f'{file_path}combined_data.csv', index=False)
-print(merged_df)
+print(merged_df.head(5))
 
--- model_check.py
+++ model_check.py
@@ -3,22 +3,39 @@ import joblib
 
 # Input data for prediction
 input_data = {
-    'open': 58.0,
-    'high': 58.12,
-    'low': 57.4,
-    'volume': 6690000,
-    'overall_sentiment_score': 0.5  # Provide a sentiment score based on your criteria
+      "priceDate": 1704412800000000000,
+      "open": 236.86,
+      "low": 235.5001,
+      "high": 244.1196,
+      "volume": 93488939.0,
+      "Revenues": 23350000000,
+      "NetIncomeLoss": 1853000000,
+      "EarningsPerShareBasic": 0.58,
+      "EarningsPerShareDiluted": 0.53,
+      "OperatingIncomeLoss": 1764000000,
+      "CostOfRevenue": 19172000000,
+      "GrossProfit": 4178000000,
+      "CashAndCashEquivalentsAtCarryingValue": 15932000000,
+      "LongTermDebt": 2152000000,
+      "DebtCurrent": 1552000000,
+      "Assets": 93941000000,
+      "LiabilitiesCurrent": 26640000000,
+      "InventoryNet": 13721000000,
+      "ResearchAndDevelopmentExpense": 1161000000
 }
 
 # Create a DataFrame from the input data
 input_df = pd.DataFrame([input_data])
 
+# Reshape input data to match the model's expected input shape
+input_array = input_df.values.reshape((input_df.shape[0], 1, input_df.shape[1]))
+
 # Load the trained model
-model_filename = './models/stock_price_prediction_model.joblib'
+model_filename = './models/TSLA_iexcloud_basic_rnn.joblib'
 model = joblib.load(model_filename)
 
 # Make predictions for the input data
-predicted_close_price = model.predict(input_df[['open', 'high', 'low', 'volume', 'overall_sentiment_score']])
+predicted_close_price = model.predict(input_array)
 
 # Display the predicted close price
-print(f'Predicted Close Price: ${predicted_close_price[0]:.2f}')
+print(f'Predicted Close Price: ${predicted_close_price[0][0]:.2f}')
